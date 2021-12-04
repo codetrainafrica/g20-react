835c795ef38af9fcb500f00cf66c4c2786a92e99
Anthony Obaze <toneyobaze@gmail.com>
2021-12-04 14:13:35 +0000

    added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello</h1>;
+//   }
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Navbar />
+      <Side />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+function Side() {
+  return (
+    <div style={{ backgroundColor: "yellow" }}>
+      <h1>Side</h1>
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+const Body = () => {
+  return (
+    <div>
+      <h1>Body</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default Body;
