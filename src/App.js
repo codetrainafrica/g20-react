ac236baa5c411a37832ca11e2fb65f44f98ec5e5
Anthony Obaze <toneyobaze@gmail.com>
2021-12-09 19:53:00 +0000

    implement state and forms

--- src/App.js
+++ src/App.js
@@ -1,32 +1,116 @@
-import React from "react";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
+import React, { useState } from "react";
+import "./App.css";
 
 // class App extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.state = { number: 0, name: "kofi", age: 20 };
+//   }
+
+//   addOne = () => {
+//     this.setState({ number: this.state.number + 1 });
+//   };
+
 //   render() {
-//     return <h1>Hello</h1>;
+//     return (
+//       <div className="App">
+//         <h1>{this.state.number}</h1>
+//         <button onClick={this.addOne}>Add 1</button>
+//         <button>Subtract 1</button>
+//       </div>
+//     );
+//   }
+// }
+
+// function App() {
+//   const [number, setNumber] = useState(0);
+
+//   const addOne = () => {
+//     setNumber(number + 1);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>{number}</h1>
+//       <button onClick={addOne}>Add 1</button>
+//     </div>
+//   );
+// }
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.state = { email: "", password: "" };
+//   }
+
+//   handleEmailChange = (event) => {
+//     this.setState({ email: event.target.value });
+//   };
+
+//   handlePasswordChange = (event) => {
+//     this.setState({ password: event.target.value });
+//   };
+
+//   handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     console.log(this.state.email, this.state.password);
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <form onSubmit={this.handleSubmit}>
+//           <input
+//             type="email"
+//             value={this.state.email}
+//             onChange={this.handleEmailChange}
+//           />
+//           <input
+//             type="password"
+//             value={this.state.password}
+//             onChange={this.handlePasswordChange}
+//           />
+//           <button type="submit">Submit</button>
+//         </form>
+//       </div>
+//     );
 //   }
 // }
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <Navbar name="Tony" />
-      <Sidebar />
-      <Main />
-      <Footer />
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="email" value={email} onChange={handleEmailChange} />
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
-}
-
-class Navbar extends React.Component {
-  render() {
-    return <h1 style={{ backgroundColor: "red" }}>{this.props.name}</h1>;
-  }
-}
-
-function Sidebar() {
-  return <h1 style={{ backgroundColor: "blue" }}>Sidebar</h1>;
 }
 
 export default App;
